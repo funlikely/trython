f88042db24404abd9b8abbeca73fdc9dd43702b0
Oldflat <ducksalt+github@gmail.com>
2021-01-28 22:16:54 -0500

    Mandelbrot processing update - Divide screen into blocks. Could lend itself to parallel processing.

--- DrawMandelbrotSet/DrawMandelbrotSet.py
+++ DrawMandelbrotSet/DrawMandelbrotSet.py
@@ -8,35 +8,71 @@ import math
 import cmath
 
 
+def create_mandelbrot_block(mandelbrot_size, mandelbrot_center, screen_size, block_size, block_start) -> pygame.Surface:
+    surf = pygame.Surface(block_size)
+
+    scale_x = mandelbrot_size[0] / screen_size[0]
+    scale_y = mandelbrot_size[1] / screen_size[1]
+
+    screen_scoot_x = block_start[0]
+    screen_scoot_y = block_start[1]
+
+    for i in range(surf.get_width()):
+        for j in range(surf.get_height()):
+            scaled_down_x = (screen_scoot_x + i) / screen_size[0]
+            scaled_up_x = scaled_down_x * mandelbrot_size[0]
+            x = scaled_up_x - (mandelbrot_size[0]-mandelbrot_center[0]) / 2
+            scaled_down_y = (screen_scoot_y + j) / screen_size[1]
+            scaled_up_y = scaled_down_y * mandelbrot_size[1]
+            y = scaled_up_y -(mandelbrot_size[1] - mandelbrot_center[1]) /2
+
+            layer, is_mandelbrot = mandelbrot(x, y, 50)
+
+            if is_mandelbrot:
+                color =  0, 0, 0
+            else:
+                color =  get_color_by_mandelbrot_layer(layer)
+
+            surf.set_at((i, j), color)
+
+    return surf
+
+
 def main():
     pygame.init()
 
-    width = 1400
-    height = 700
-    screen = pygame.display.set_mode((width, height))
+    screen_size = (1600, 960)
+    block_size = (100, 55)
+    screen = pygame.display.set_mode(screen_size)
 
-    dirty = True
+    mandelbrot_size = (4,2.4)
+    mandelbrot_center = (-0.5,0)
+    time_count = 0
 
     while 1:
-
-        if dirty:
-            screen.blit(screen, (0, 0, screen.get_width(), screen.get_height()))
-
-            try:
-                for j in range(height):
-                    for i in range(width):
-                        color = get_my_surface_color(width, height, i, j)
-                        screen.set_at((i, j), color)
-                    if j % 20 == 0:
-                        pygame.display.update()
-            except:
-                print("an exception occurred")
-        dirty = False
-        # pygame.display.flip()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        screen.blit(screen, (0, 0, screen.get_width(), screen.get_height()))
+
+        for j in range(1, screen_size[1], block_size[1]):
+            for i in range(1, screen_size[0], block_size[0]):
+
+
+                # surface_to_color = pygame.Surface((block_width, block_height))
+                # surface_to_color.fill(((i*53 + j*7 + time_count) % 255, (i * 11 + j *17 + time_count/2) % 255, (i*23 + j*19 + time_count/3) % 255))
+
+                mandelbrot_block = create_mandelbrot_block(mandelbrot_size, mandelbrot_center, screen_size, block_size, (i,j))
+
+                screen.blit(mandelbrot_block, (i, j, i + block_size[0], j + block_size[1]))
+
+                pygame.display.update()
+                # color = get_my_surface_color(screen_width, screen_height, i, j)
+                # screen.set_at((i, j), color)
+
+        time_count +=1
 
 
 def colorize_screen_with_mandelbrot(screen):

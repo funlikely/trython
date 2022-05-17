789efff418faef57dec07e876c741bee840424d7
Oldflat <ducksalt+github@gmail.com>
2022-05-16 21:52:27 -0400

    Project Euler 9-12 refactor

--- ProjectEuler/project_euler_010_summation_of_primes.py
+++ ProjectEuler/project_euler_010_summation_of_primes.py
@@ -1,3 +1,11 @@
+# Summation of primes
+#
+# Problem 10
+#
+# The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+#
+# Find the sum of all the primes below two million.
+
 def primeSieveUpTo(n):
     sieve = [True] * n
     sieve[0] = sieve[1] = False

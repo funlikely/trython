1de433c8d93a6c4872808b05495585edf37f1646
Oldflat <ducksalt+github@gmail.com>
2022-05-16 20:29:05 -0400

    Project Euler 6-8 refactor

--- ProjectEuler/project_euler_007_10001st_prime.py
+++ ProjectEuler/project_euler_007_10001st_prime.py
@@ -1,3 +1,12 @@
+# 10001st prime
+#
+# Problem 7
+#
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+#
+# What is the 10 001st prime number?
+
+
 def primeSieve(n):
     sieve = [True] * n * int(n ** (0.5) + 1)
     sieve[0] = sieve[1] = False
--- ProjectEuler/project_euler_6.py
+++
@@ -1,7 +0,0 @@
-sumOfSquare = 0
-ourSum = 0
-for i in range(1, 101):
-    sumOfSquare += i*i
-    ourSum += i
-print(ourSum*ourSum - sumOfSquare)
-
